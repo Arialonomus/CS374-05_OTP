b9a6f03b6a25ee9a0e5aa19c11b47169c08a863c
Jacob Barber <barber.jacobervin@gmail.com>
2024-03-15 18:28:02 -0500

    Added reap() to handle zombie processes

--- headers/server.h
+++ headers/server.h
@@ -13,8 +13,18 @@
 #define QUEUE_LIMIT 5
 #endif
 
+#include <err.h>
+#include <errno.h>
+#include <signal.h>
+#include <stddef.h>
+#include <sys/wait.h>
+
 #include "cryptography.h"
 
+// Receive a key and plaintext from a client and send back an encrypted string
 void handle_encryption(void);
+
+// Signal handler to reap all zombie processes
+void reap(int sig);
 
 #endif //OTP_SERVER_H
--- source/shared/server.c
+++ source/shared/server.c
@@ -4,3 +4,11 @@ void handle_encryption()
 {
 
 }
+
+void reap(int sig)
+{
+    const int saved_err = errno;
+    while (waitpid(-1, NULL, WNOHANG) > 0)
+        continue;
+    errno = saved_err;
+}
