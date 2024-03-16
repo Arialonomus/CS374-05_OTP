e033d8b5d79ee1f2acebd3b11064d2c99840214c
Jacob Barber <barber.jacobervin@gmail.com>
2024-03-15 20:47:18 -0500

    Moved definition of MAX_PORT_NUMBER to address.h so it can be shared between clients and servers

--- headers/address.h
+++ headers/address.h
@@ -1,8 +1,8 @@
-/* This file contains functions related to address handling for network sockets
+/* This file contains functions and constants related to address handling for network sockets
  */
 
-#ifndef ADDRESS_H
-#define ADDRESS_H
+#ifndef OTP_ADDRESS_H
+#define OTP_ADDRESS_H
 
 #include <stdio.h>
 #include <string.h>
@@ -13,11 +13,4 @@
 #define MAX_PORT_NUMBER 65535
 #endif
 
-#ifndef BACKLOG
-#define MAX_PORT_NUMBER 5
-#endif
-
-// Set up the address struct for the server socket
-void configure_address(struct sockaddr_in* address, int port_num);
-
-#endif //ADDRESS_H
+#endif //OTP_ADDRESS_H
--- headers/server.h
+++ headers/server.h
@@ -5,10 +5,6 @@
 #ifndef OTP_SERVER_H
 #define OTP_SERVER_H
 
-#ifndef  MAX_PORT_NUMBER
-#define MAX_PORT_NUMBER 65535
-#endif
-
 #ifndef QUEUE_LIMIT
 #define QUEUE_LIMIT 5
 #endif
@@ -26,7 +22,7 @@
 void handle_encryption(int socket_fd);
 
 // Fetches a pair of characters from a socket stream, returns 2 on success, 0 on EOF, otherwise returns -1
-int fetch_pair(int fd, char pair_buf[])
+int fetch_pair(int fd, char pair_buf[]);
 
 // Signal handler to reap all zombie processes
 void reap(int sig);
